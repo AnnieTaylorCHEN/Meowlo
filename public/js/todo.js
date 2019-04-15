f497344c966ce3464a3a0e09e9f9c622c87f38a8
Web Dev Simplified <webdevsimplified@gmail.com>
2019-04-15 16:27:58 -0500

    Todo Changes

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,182 +1,146 @@
-//set filters
-const filters = {
-    searchText: '',
-    hideCompleted: false
-}
-
-const getFilters = () => filters
-
-const setFilters = (updates) => {
-    if (typeof updates.searchText === 'string'){
-        filters.searchText = updates.searchText
-    } 
-    if (typeof updates.hideCompleted === 'boolean'){
-        filters.hideCompleted = updates.hideCompleted
+class TodoList {
+    constructor(todoListElement, emptyMessageElement, summaryElement) {
+        this.todoListElement = todoListElement
+        this.emptyMessageElement = emptyMessageElement
+        this.summaryElement = summaryElement
+        this.todos = this.getTodosFromStorage()
+        this.hideCompleted = false
+        this.searchText = ''
     }
-} 
 
-//create todo, loadtodos
-
-let todos = []
-
-const loadTodos = () => {
-    const todosJSON = localStorage.getItem('todos')
-
-    try {
-        return todosJSON ? JSON.parse(todosJSON) : []
-    } catch (e) {
-        return []
-    }
-}
-
-const saveTodos = () => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-}
-
-const getTodos = () => todos
-
-
-const createTodo = (text) => {
-        todos.push({
-            id: uuidv4(),
-            text: text,
-            completed: false
+    get filteredTodos() {
+        return this.todos.filter(todo => {
+            const hideCompletedMatch = !this.hideCompleted || !todo.completed
+            const searchTextMatch = todo.text.toLowerCase().includes(this.searchText.toLowerCase())
+            return searchTextMatch && hideCompletedMatch
         })
-        saveTodos()
-}
-
-const removeTodo = (id) => {
-    const todoIndex = todos.findIndex((todo) => todo.id === id)
-
-    if (todoIndex > -1) {
-        todos.splice(todoIndex, 1)
-        saveTodos()
     }
-}
 
-const toggleTodo = (id) => {
-    const todo = todos.find((todo) => todo.id === id)
+    getTodosFromStorage() {
+        const todosJSON = localStorage.getItem('todos')
 
-    if (todo) {
+        try {
+            return todosJSON ? JSON.parse(todosJSON) : []
+        } catch (e) {
+            return []
+        }
+    }
+
+    createTodo(text) {
+        this.todos.push({ id: uuidv4(), text: text, completed: false })
+        this.saveTodos()
+    }
+
+    saveTodos() {
+        localStorage.setItem('todos', JSON.stringify(this.todos))
+    }
+
+    removeTodo(id) {
+        this.todos = this.todos.filter(todo => todo.id !== id)
+        this.saveTodos()
+    }
+
+    toggleTodo(id) {
+        const todo = this.todos.find((todo) => todo.id === id)
+        if (todo == null) return
         todo.completed = !todo.completed
-        saveTodos()
+        this.saveTodos()
     }
-}
 
-loadTodos()
-
-//render DOM interaction
-const renderTodos = () => {
-    const todoEl = document.querySelector('#todos')
-    const filters = getFilters()
-    const filteredTodos = getTodos().filter((todo) => {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
-        
-        return searchTextMatch && hideCompletedMatch
-    })
-    const incompleteTodos = filteredTodos.filter((todo) => !todo.completed)
-
-    todoEl.innerHTML = ''
-    todoEl.appendChild(generateSummaryDOM(incompleteTodos))
-
-    if (filteredTodos.length > 0) {
-        filteredTodos.forEach((todo) => {
-            todoEl.appendChild(generateTodoDOM(todo))
+    renderTodos() {
+        this.todoListElement.innerHTML = ''
+        const incompleteTodos = this.filteredTodos.filter(todo => !todo.complete)
+        this.summaryElement.innerText = this.getSummaryText(incompleteTodos)
+        this.todoListElement.appendChild(this.summaryElement)
+            this.filteredTodos.forEach(todo => {
+            this.todoListElement.appendChild(this.generateTodoDOM(todo))
         })
-    } else {
-        const messageEl = document.createElement('p')
-        messageEl.classList.add('empty-message')
-        messageEl.textContent = 'No to-dos to show'
-        todoEl.appendChild(messageEl)
+
+        if (this.filteredTodos.length <= 0) {
+            this.todoListElement.appendChild(this.emptyMessageElement)
+        }
+    }
+
+    generateTodoDOM(todo) {
+        const todoEl = document.createElement('label')
+        const containerEl = document.createElement('div')
+        const checkbox = document.createElement('input')
+        const todoText = document.createElement('p')
+        const removeButton = document.createElement('button')
+
+        // Setup todo checkbox
+        checkbox.setAttribute('type', 'checkbox')
+        checkbox.checked = todo.completed
+        containerEl.appendChild(checkbox)
+        checkbox.classList.add('checkbox--inner')
+        checkbox.addEventListener('change', () => {
+            this.toggleTodo(todo.id)
+            this.renderTodos()
+        })
+
+        // Setup the todo text
+        todoText.textContent = todo.text
+        containerEl.appendChild(todoText)
+
+        // Setup container
+        todoEl.classList.add('list-item')
+        containerEl.classList.add('list-item__container')
+        todoEl.appendChild(containerEl)
+
+        // Setup the remove button
+        removeButton.textContent = 'remove'
+        removeButton.classList.add('button', 'button--danger')
+        todoEl.appendChild(removeButton)
+        removeButton.addEventListener('click', () => {
+            this.removeTodo(todo.id)
+            this.renderTodos()
+        })
+
+        return todoEl
+    }
+
+    getSummaryText(incompleteTodos) {
+        const plural = incompleteTodos.length === 1 ? '' : 's'
+        return `You have ${incompleteTodos.length} todo${plural} left`
     }
 }
 
-
-const generateTodoDOM = (todo) => {
-    const todoEl = document.createElement('label')
-    const containerEl = document.createElement('div')
-    const checkbox = document.createElement('input')
-    const todoText = document.createElement('p')
-    const removeButton = document.createElement('button')
-
-    // Setup todo checkbox
-    checkbox.setAttribute('type', 'checkbox')
-    checkbox.checked = todo.completed
-    containerEl.appendChild(checkbox)
-    checkbox.classList.add('checkbox--inner')
-    checkbox.addEventListener('change', () => {
-        toggleTodo(todo.id)
-        renderTodos()
-    })
-
-    // Setup the todo text
-    todoText.textContent = todo.text
-    containerEl.appendChild(todoText)
-
-    // Setup container
-    todoEl.classList.add('list-item')
-    containerEl.classList.add('list-item__container')
-    todoEl.appendChild(containerEl)
-
-    // Setup the remove button
-    removeButton.textContent = 'remove'
-    removeButton.classList.add('button', 'button--danger')
-    todoEl.appendChild(removeButton)
-    removeButton.addEventListener('click', () => {
-        removeTodo(todo.id)
-        renderTodos()
-    }) 
-
-    return todoEl
-}
-
-
-const generateSummaryDOM = (incompleteTodos) => {
-    const summary = document.createElement('h2')
-    const plural = incompleteTodos.length === 1 ? '' : 's'
-    summary.classList.add('list-title')
-    summary.textContent = `You have ${incompleteTodos.length} todo${plural} left`
-    return summary
-}
-
-//Set up interaction
-
-
-renderTodos()
+const todoListElement = document.getElementById('todos')
+const emptyMessageElement = document.createElement('p')
+emptyMessageElement.classList.add('empty-message')
+emptyMessageElement.textContent = 'No to-dos to show'
+const summaryElement = document.createElement('h2')
+summaryElement.classList.add('list-title')
+const todoList = new TodoList(todoListElement, emptyMessageElement, summaryElement)
+todoList.renderTodos()
 
 // Set up search text handler
-document.querySelector('#search-text').addEventListener('input', (e) => {
-    setFilters({
-        searchText: e.target.value
-    })
-    renderTodos()
+document.getElementById('search-text').addEventListener('input', (e) => {
+    todoList.searchText = e.target.value
+    todoList.renderTodos()
 })
 
 // Set up checkbox handler
-document.querySelector('#hide-completed').addEventListener('change', (e) => {
-    setFilters({
-        hideCompleted: e.target.checked
-    })
-    renderTodos()
+document.getElementById('hide-completed').addEventListener('change', (e) => {
+    todoList.hideCompleted = e.target.checked
+    todoList.renderTodos()
 })
 
 // Set up form submission handler
-document.querySelector('#new-todo').addEventListener('submit', (e) => {
-    const text = e.target.elements.text.value.trim()
+document.getElementById('new-todo').addEventListener('submit', (e) => {
     e.preventDefault()
-        if (text.length > 0){
-            createTodo(text)
-            renderTodos()
-            e.target.elements.text.value = ''
-        }
-    })
+    const text = e.target.elements.text.value.trim()
+    if (text.length <= 0) return
+    todoList.createTodo(text)
+    todoList.renderTodos()
+    e.target.elements.text.value = ''
+})
 
 
 // Bonus: Add a watcher for local storage
 window.addEventListener ('storage', (e)=>{
     if (e.key === todos){
-        loadTodos()
-        renderTodos()
+        todoList.getTodosFromStorage()
+        todoList.renderTodos()
     }
 })
